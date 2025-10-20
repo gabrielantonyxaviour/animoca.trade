dc59177aa9ef849f9322fc613138ab5cebc7b0e2
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2025-10-21 00:38:13 +0530

    fix: add .ts extension to utils imports for Vercel build compatibility

--- frontend/src/components/ui/progress.tsx
+++ frontend/src/components/ui/progress.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils.ts"
 
 export interface ProgressProps extends React.HTMLAttributes<HTMLDivElement> {
   value?: number
